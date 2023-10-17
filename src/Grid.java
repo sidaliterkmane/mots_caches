5358ad6cf4d5c2944045ca7b92ada5ec7a3f2e3c
Sid Ali Terkmane <sidterkmane@hotmail.com>
2023-10-17 13:07:23 -0400

    started main structure of Grid class

--- src/Grid.java
+++ src/Grid.java
@@ -1,2 +1,19 @@
 public class Grid {
+    private int M; // Nombre de lignes
+    private int N; // Nombre de colonnes
+    private char[][] grid;
+
+    public Grid(int M, int N, char[][] grid) {
+        this.M = M;
+        this.N = N;
+        this.grid = grid;
+    }
+
+    public int getM() {
+        return M;
+    }
+
+    public int getN() {
+        return N;
+    }
 }
