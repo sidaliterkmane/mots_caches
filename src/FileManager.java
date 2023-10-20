c926866a4c079b1ed9c846983ef684c5903a8cd1
Sid Ali Terkmane <sidterkmane@hotmail.com>
2023-10-20 15:06:46 -0400

    FileManager class can now read grids and wordlists and store them

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,39 +1,64 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
-
-//Sinon on peut utiliser un scanner? Pour utiliser nextInt par exemple
+import java.util.Arrays;
+import java.util.List;
 
 public class FileManager{
+    private String fileName;
+    private List<Grid> grids;
+    private List<List<String>> wordLists;
 
-    private static void file(String[] args){
+    public FileManager(String fileName) {
+        this.fileName = fileName;
+        this.grids = new ArrayList<>();
+        this.wordLists = new ArrayList<>();
+    }
 
-        String fileName = args[0];
+    public void parseInputFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
 
-        //créer un tableau dans lequel on ajoute soit une case par
-        //line ou autre type de tableau?
+            // Lire le fichier jusqu'a ce qu'il n'y ait plus de lignes
+            while ((line = reader.readLine()) != null) {
+                // Separer les dimensions en 2 int pour rangees et cols
+                String[] dimensions = line.split(" ");
+                int rows = Integer.parseInt(dimensions[0]);
+                int cols = Integer.parseInt(dimensions[1]);
 
-        try{
+                // Creer une nouvelle grille avec les dimensions donnees
+                Grid grid = new Grid(rows, cols);
 
-            Scanner scan = new Scanner(new File(fileName));
-            int i = 1; //numero de grille
-            ArrayList<Grid> gridTable = new ArrayList<Grid>();
+                // Lire les prochaines rangees*colonnes pour remplir la grille
+                for (int i = 0; i < rows; i++) {
+                    line = reader.readLine();
+                    for (int j = 0; j < cols; j++) {
+                        grid.setCell(i, j, line.charAt(j * 2)); // j * 2 pour skip les espaces
+                    }
+                }
 
-            while (scan.hasNext()){
+                grids.add(grid);
 
-                String gridName= "Grid" + i;
-                int nbCol = scan.nextInt();
-                int nbLines = scan.nextInt();
-                int j=1; //pour savoir quand on arrive à la fin d'une grille
+                // Lis la prochaine ligne qui contient la collection de mots
+                line = reader.readLine();
 
+                List<String> words = new ArrayList<>(Arrays.asList(line.split(" ")));
 
+                // Ajoute les listes de mots a la liste de liste de mots
+                wordLists.add(words);
             }
-
-
-        }   catch (FileNotFoundException ex){
-            System.out.println("Erreur à l'ouverture du fichier");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
+    public List<Grid> getGrids() {
+        return grids;
+    }
+
+    public List<List<String>> getWordLists() {
+        return wordLists;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.io.File;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // S'assurer que le nom du ficher est correctement donne
+        if (args.length < 1) {
+            System.out.println("Please provide the path to the input file.");
+            return;
+        }
+
+        String filePath = args[0];
+
+        // Cree une instance de fileManager et itere a travers le fichier input
+        FileManager fileManager = new FileManager(filePath);
+        fileManager.parseInputFile();
+
+        // Collecte les grilles et les listes de mots
+        List<Grid> grids = fileManager.getGrids();
+        List<List<String>> wordLists = fileManager.getWordLists();
+
+        // Imprime les grilles et les listes de mots
+        for (int i = 0; i < grids.size(); i++) {
+            printGrid(grids.get(i));
+            System.out.println("Words to search : " + wordLists.get(i));
+            System.out.println("---------");
+        }
     }
+
+    // Helper method pour imprimer une grid
+    private static void printGrid(Grid grid) {
+        for (int i = 0; i < grid.getRows(); i++) {
+            for (int j = 0; j < grid.getCols(); j++) {
+                System.out.print(grid.getCell(i, j).getValue() + " ");
+            }
+            System.out.println();  // Move to the next line after printing a row
+        }
+    }
+
 }
