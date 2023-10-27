c0d75893017a6352eed43d6f0b74bb3b5c7704a9
leaneLavigne <leane.lavigne@umontreal.ca>
2023-10-26 21:06:26 -0400

    fin des commentaires et méthodes helpers enlevés

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+// Packages utilisés dans "Main".
 import java.io.File;
 import java.util.List;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        // S'assurer que le nom du ficher est correctement donne
+
+        // Pour s'assurer que le nom du ficher est correctement donné.
         if (args.length < 1) {
             System.out.println("Please provide the path to the input file.");
             return;
@@ -12,52 +14,31 @@ public class Main {
 
         String filePath = args[0];
 
-        // Cree une instance de fileManager et itere a travers le fichier input
+        // Création d'une instance de fileManager et itération à travers le fichier d'entré.
         FileManager fileManager = new FileManager(filePath);
         fileManager.parseInputFile();
 
-        // Collecte les grilles et les listes de mots
+        // Pour emmagasiner les grilles et les listes de mots.
         List<Grid> grids = fileManager.getGrids();
         List<List<String>> wordLists = fileManager.getWordLists();
 
-        // Pour chaque grille et liste de mots, execute la recherche de mots et imprime le resultat
+        // Pour chaque grille et liste de mots, la recherche de mots est exécutée et le resultat
+        // est imprimé.
         for (int i = 0; i < grids.size(); i++) {
             Grid grid = grids.get(i);
             List<String> wordList = wordLists.get(i);
 
             WordSearch wordSearch = new WordSearch(grid, wordList);
 
-            // Execute la recherche de mots
+            // Exécution de la recherche de mots.
             TreeSet<String> results = wordSearch.findWords();
 
-            // Imprime le resultat dans le format specifie dans le TP
+            // Pour imprimer le resultat dans le format specifié dans l'énoncé du TP.
             System.out.println("Query " + (i + 1) + ":");
             for (String result : results) {
                 System.out.println(result);
             }
-            System.out.println();  // Separe les resultats des differents queries avec une ligne vide
-        }
-    }
-
-    // Helper method pour imprimer une grille
-    private static void printGrid(Grid grid) {
-        for (int i = 0; i < grid.getRows(); i++) {
-            for (int j = 0; j < grid.getCols(); j++) {
-                System.out.print(grid.getCell(i, j).getValue() + " ");
-            }
-            System.out.println();  // Saute une ligne apres avoir imprime une rangee
-        }
-        // Ex: Imprime neighbors pour la cellule (1,1)
-        printNeighbors(grid, grid.getCell(1, 1));
-    }
-
-
-    // Helper method qui imprime les voisins d'une cellule donnee
-    private static void printNeighbors(Grid grid, Cell cell) {
-        List<Cell> neighbors = grid.getNeighbors(cell);
-        System.out.println("Neighbors of cell (" + cell.getRow() + ", " + cell.getCol() + ") with value '" + cell.getValue() + "':");
-        for (Cell neighbor : neighbors) {
-            System.out.println("(" + neighbor.getRow() + ", " + neighbor.getCol() + ") -> '" + neighbor.getValue() + "'");
+            System.out.println();  // Separe les résultats des differents queries avec une ligne vide.
         }
     }
 }
--- src/WordSearch.java
+++ src/WordSearch.java
@@ -1,53 +1,63 @@
+// Packages que nous utilisons dans la classe "WordSearch".
 import DS.Trie;
 import java.util.List;
 import java.util.TreeSet;
 
+// Classe allant nous permettre de trouver la solution des mots cachés.
 public class WordSearch {
-    private Grid grid;
-    private Trie trie;
+    private Grid grid;  // Grille à résoudre. 
+    private Trie trie;  // Trie pour trouver les solutions.
 
-    // Constructor
+    // Constructeur.
     public WordSearch(Grid grid, List<String> wordList) {
+
         this.grid = grid;
-        // Convertit wordList en un array et instatie le Trie
-        // Puis nous allons comparer chaque cellule de la grille avec un noeud du Trie, si ca match,
-        // on continue la recherche, sinon, on s'arrete
+
+        // Pour convertit wordList en un array et instancier le Trie.
         this.trie = new Trie(wordList.toArray(new String[0]));
     }
 
+    // Puis nous allons comparer chaque cellule de la grille avec un noeud du Trie, si ca match,
+    // on continue la recherche, sinon, on s'arrete
+
     /**
-     * Methode recursive qui traverse une grille et cherche les mots
-     * @param i - rangee actuelle
-     * @param j - colonne actuelle
+     * Méthode récursive qui traverse une grille et cherche les mots.
+     * @param i - Rangée actuelle
+     * @param j - Colonne actuelle
      * @param node - TrieNode actuelle
-     * @param path - chemin prit pour arriver a la Cell actuelle
-     * @param results - resultats dans le format requis, dans un TreeSet ils sont ordonnes lexicographiquement
+     * @param path - Chemin prit pour arriver à la cellule actuelle
+     * @param results - Résultats dans le format requis (ordonné lexicographiquement)
      */
-    private void search(int i, int j, Trie.TrieNode node, StringBuilder path, StringBuilder currentWord, TreeSet<String> results) {
+    private void search(int i, int j, Trie.TrieNode node, StringBuilder path, StringBuilder currentWord, 
+    TreeSet<String> results) {
+
         char currentChar = grid.getCell(i, j).getValue();
 
-        // Verifie si le caractere de la cellule actuelle n'est pas present dans le Trie
+        // Vérifie si le caractère de la cellule actuelle n'est pas présent dans le Trie.
         if (!node.children.containsKey(currentChar)) {
             return;
         }
 
-        // Stringbuilder permet de plus facilement manipuler les strings
-        StringBuilder newPath = new StringBuilder(path); // Clone le chemin actuel
+        // Mise à jour du chemin avec une nouvelle cellule.
+        StringBuilder newPath = new StringBuilder(path); 
         newPath.append("(").append(i).append(",").append(j).append(")->");
 
+        // Ajout du caractère du mot actuellement cherché.
         currentWord.append(currentChar);
-
+        
+        // Pour que le préfixe du mot recherché contenant le caractère actuel devienne la valeur
+        // associé au prochain noeud.
         Trie.TrieNode nextNode = node.children.get(currentChar);
 
-        // Si un mot valide est forme
+        // Vérifie si un mot valide est formé.
         if (nextNode.isWord) {
-            results.add(currentWord.toString() + " " + newPath.substring(0, newPath.length() - 2)); // enleve le dernier "->"
+            results.add(currentWord.toString() + " " + newPath.substring(0, newPath.length() - 2)); // Enlève le dernier "->".
         }
 
-        // Cherche les voisins de la cellule actuelle, incluant elle-meme
+        // Cherche les voisins de la cellule actuelle, incluant elle-même.
         List<Cell> neighbors = grid.getNeighbors(grid.getCell(i, j));
 
-        // Pour chaque voisin, (incluant la cellule elle-meme), faire un appel recursif
+        // Pour chaque voisin, (incluant la cellule elle-même), faire un appel recursif de la méthode "search".
         for (Cell neighbor : neighbors) {
             search(neighbor.getRow(), neighbor.getCol(), nextNode, new StringBuilder(newPath),
                     new StringBuilder(currentWord), results);
@@ -55,16 +65,17 @@ public class WordSearch {
     }
 
     /**
-     * Initie la recherche de mot a partir de chaque cellule de la grille
+     * Initie la recherche de mot à partir de chaque cellule de la grille.
      *
-     * @return TreeSet contenant les mots trouves et leur chemin (en ordre lexicographique)
+     * @return TreeSet contenant les mots trouvés et leur chemin (en ordre lexicographique)
      */
     public TreeSet<String> findWords() {
         TreeSet<String> results = new TreeSet<>();
         int rows = grid.getRows();
         int cols = grid.getCols();
 
-        // Commence la recherche a partir de chaque cellule
+        // Commence la recherche à partir de chaque colonne (en ordre croissant) de la première 
+        // rangée jusqu'à la dernière rangée.
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 search(i, j, trie.root, new StringBuilder(), new StringBuilder(), results);
